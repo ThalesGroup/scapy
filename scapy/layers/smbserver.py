32668065aff6401d4dd418419fb90df12485ff89
Alexis ROYER <alexis.royer@thalesgroup.com>
2025-09-17 09:54:11 +0200

    Log things in `SMB_Server.receive_tree_connect()`

--- scapy/layers/smbserver.py
+++ scapy/layers/smbserver.py
@@ -764,7 +764,10 @@ class SMB_Server(Automaton):
     @ATMT.receive_condition(SERVING)
     def receive_tree_connect(self, pkt):
         if SMB2_Tree_Connect_Request in pkt:
+            self.vprint(f"SMB_Server.receive_tree_connect(): pkt={pkt!r}")
+            pkt.show()
             tree_name = pkt[SMB2_Tree_Connect_Request].Path.split("\\")[-1]
+            self.vprint(f"SMB_Server.receive_tree_connect(): tree_name={tree_name!r}")
             raise self.SERVING().action_parameters(pkt, tree_name)
 
     @ATMT.action(receive_tree_connect)
